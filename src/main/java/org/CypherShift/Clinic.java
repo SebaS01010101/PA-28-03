9fcc6a5dffdc0cbe2431a903456ae646410017cf
SebaS01010101 <s.sandoval18@ufromail.cl>
2025-03-31 22:40:35 -0300

    chore: add missing constructors and setters in clinical entities

--- src/main/java/org/CypherShift/Clinic.java
+++ src/main/java/org/CypherShift/Clinic.java
@@ -1,4 +1,17 @@
 class Clinic{
     private String name;
     private String adress;
+
+    public Clinic(String name, String adress) {
+        this.name = name;
+        this.adress = adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/org/CypherShift/Patient.java
+++ src/main/java/org/CypherShift/Patient.java
@@ -2,4 +2,22 @@ class Patient{
     private int id;
     private String name;
     private String numberPhone;
+
+    public Patient(int id, String name, String numberPhone) {
+        this.id = id;
+        this.name = name;
+        this.numberPhone = numberPhone;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumberPhone(String numberPhone) {
+        this.numberPhone = numberPhone;
+    }
 }
